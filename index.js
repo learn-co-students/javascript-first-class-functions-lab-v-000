9ae5a48027099c0134ce335f7a5a61e77b4f7fea
James Novak <jamesnovak90@gmail.com>
2017-01-07 15:04:46 -0500

    completed create multiplier function with doubler and tripler variables

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function countdown(callback){
   window.setTimeout(callback, 2000)
 }
+
+function createMultiplier(multiplierValue){
+  return function(value){
+    return multiplierValue * value
+  }
+}
+
+var doubler = createMultiplier(2)
+var tripler = createMultiplier(3)
