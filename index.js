fe24bd98a8729861146eebabc10bef4630fa2e56
bmolina-nyc <bruce.molina.81@gmail.com>
2017-02-23 20:30:31 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function countdown(callback){
+  window.setTimeout(callback, 2000)
+}
+
+// function alert(){
+//   console.log("waited two seconds")
+// }
+
+// countdown(alert)
+
+function createMultiplier(multiplierValue){
+
+
+  return function(e){
+    console.log(parseInt(e) * multiplierValue)
+  }
+}
+
+var doubler = createMultiplier(2)
+var tripler = createMultiplier(3)
+
+
+
+function multiplier(arg1, arg2){
+  return arg1 * arg2
+}
+
+var doublerWithBind = multiplier.bind(2,null)
+var triplerWithBind = multiplier.bind(3, null)
