4f87f1a2f4b2e87d157941cc2ed3bb33938398f8
Bryan Hou <bryan.hou94@gmail.com>
2017-04-12 05:04:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function countdown(callback) {
+  setTimeout(function (){callback();},2000);
+}
+
+function createMultiplier(multiplierValue) {
+  return function(value) {
+    return multiplierValue * value;
+  };
+}
+
+doubler = createMultiplier(2);
+tripler = createMultiplier(3);
+
+function multiplier(a, b) {
+  return a * b;
+}
+
+doublerWithBind = multiplier.bind(null,2);
+triplerWithBind = multiplier.bind(null,3);
