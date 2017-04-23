ce23013b903e4f6750a9c22d56b42ef0a27fb56f
gormanjp <jpgorman8@gmail.com>
2017-04-22 22:40:00 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function countdown(callback){
+  window.setTimeout(function(){
+    callback();
+  },2000);
+}
+
+function createMultiplier(x){
+  return function multiplierValue(y){
+    return x*y;
+  }
+}
+
+var doubler = createMultiplier(2);
+
+var tripler = createMultiplier(3);
+
+function multiplier(x, y){
+  return x*y;
+}
+
+var doublerWithBind = multiplier.bind(null, 2);
+
+var triplerWithBind = multiplier.bind(null, 3);
