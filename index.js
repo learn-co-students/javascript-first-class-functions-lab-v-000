a0fab29654d8a27fe5d68d174eefd548a3a478f6
wook kim <wooke2e@gmail.com>
2017-06-16 02:15:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 function countdown(callback) {
-  window.setTimeout(function callback(), 2000);
-});
+  window.setTimeout(function(){callback()}, 2000)
+}
 
 function createMultiplier(multiplierValue){
-  var doubler(() => multiplierValue * 2)
-  var tripler(() => muliiplierValue * 3)
-  return doubler()
-};
+  return function(number){
+    return number * multiplierValue
+  };
+}
 
+var doubler = createMultiplier(2)
+var tripler = createMultiplier(3)
 
+function multiplier(muliplierValue, value) {
+  return multiplierValue * value
+}
 
-function multiplier(multiplierValue, value){
-  var doublerWithBind = multiplier(value);
-  var triplerWithBind = multiplier(value);
-};
+var doublerWithBind = multiplier.bind(2, null)
+var triplerWithBind = multiplier.bind(3, null)
