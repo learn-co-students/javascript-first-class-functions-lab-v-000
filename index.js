647a730e358c122601119b6c69ea05c15f6ac9e4
Hima Chitalia <hima_chhag@yahoo.com>
2017-06-20 14:40:23 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function countdown(callback) {
+  setTimeout(() => callback(), 2000);
+}
+
+function createMultiplier(multiplierValue) {
+  return function(value) {
+    return multiplierValue*value
+  }
+}
+
+var doubler = createMultiplier(2)
+var tripler = createMultiplier(3)
+
+function multiplier(a, b){
+  console.log( a * b )
+}
+
+
+function doublerWithBind() { multiplier.bind(2)}
+function triplerWithBind() { multiplier.bind(3)}
