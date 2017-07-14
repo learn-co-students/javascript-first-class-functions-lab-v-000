cb6126ab5939b86b4f1ae71aaabcc859217d9402
lucketta <lucketta@umich.edu>
2017-07-14 17:56:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 function countdown(callback) {
-  setTimeout(callback(),2000);
-  callback();
+  setTimeout(function() {
+    callback();
+  }, 2000);
+}
+
+var doubler = createMultiplier(2)
+var tripler = createMultiplier(3)
+
+function createMultiplier(multiplierValue) {
+  return function(multiplyValue) {
+    return multiplyValue * multiplierValue;
+  };
+}
+
+var doublerWithBind = multiplier.bind(null, 2);
+var triplerWithBind = multiplier.bind(null, 3);
+
+function multiplier(multiplierValue, value) {
+  return multiplierValue * value
 }
