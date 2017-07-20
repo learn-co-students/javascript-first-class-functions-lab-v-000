58e134c52fa7a7304ee35ae8b3f3cb8b96e528b2
Aaron Samuel <aasamuelj@gmail.com>
2017-07-20 03:02:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function countdown(callback) {
+  window.setTimeout(callback, 2000);
+
+}
+
+function createMultiplier(multiplierValue) {
+  return function (multiplier) {
+    return multiplierValue * multiplier;
+  }
+}
+
+var doubler = createMultiplier(2);
+var tripler = createMultiplier(3);
+
+function multiplier(x, y) {
+  return x * y;
+}
+
+var doublerWithBind = multiplier.bind(null, 2);
+var triplerWithBind = multiplier.bind(null, 3);
