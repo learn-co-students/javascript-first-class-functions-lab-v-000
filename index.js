1212e12352c21e3888b296241765c22ae4c63b53
brownielox <mrsmpython@gmail.com>
2017-08-03 20:10:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 function countdown(callback){
-  setTimeout(callback, 2000);
+  window.setTimeout(callback, 2000);
 }
 
-function createMultiplier(multiply_by){
-  return function (value){
-    return multiply_by * value;
+function createMultiplier(input){
+  return function(value){
+    return input * value;
   };
 }
+
+// function createMultiplierBonus(input){
+//   return function(value){
+//     return input * value;
+//   };
+// }
+
 
 function multiplier(value1, value2){
   return value1 * value2;
 }
 
 var doubler = createMultiplier(2);
-var tripler = createMultipler(3);
+var tripler = createMultiplier(3);
 
 var doublerWithBind = multiplier.bind(null, 2);
 var triplerWithBind = multiplier.bind(null, 3);
