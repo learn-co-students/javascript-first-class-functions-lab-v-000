e41ffde644c993bb001de33e087a3fafcf5f7bca
Trever Andersen <trever.r.andersen@gmail.com>
2017-09-07 15:36:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+// Exercise 1 - The final countdown
+function countdown(callback) {
+ window.setTimeout(() => callback(), 2000);
+}
+
+function vocals() {
+ alert("It's the final countdown!");
+}
+
+function cue() {
+ countdown(vocals);
+}
+
+// Exercise 2 - Playing Scrabble
+function createMultiplier() {
+
+}
