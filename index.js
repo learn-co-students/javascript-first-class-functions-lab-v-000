d11141ee7eb361819bdf2503ff77a14a7231f9a7
Michael Pieschel <mpieschel39@gmail.com>
2019-06-19 18:57:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 function countdown(cb) {
     setTimeout(()=> callback(), 2000);
 }
+
+function createMultiplier(multiplyValue) {
+  return (value) => {multiplyValue * value};
+}
+
+function multiplier(a, b) {
+  return a * b;
+}
+
+var doubler = createMultiplier(2);
+var tripler = createMultiplier(3);
+var doublerWithBind = multiplier.bind(null, 2);
+var triplerWithBind = multiplier.bind(null, 3);
