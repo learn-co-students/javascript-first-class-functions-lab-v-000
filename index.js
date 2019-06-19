9e26ee7187b93f6347cd3b832faae236ea2dfbc9
Michael Pieschel <mpieschel39@gmail.com>
2019-06-19 19:07:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+
+function countdown(callback) {
+setTimeout(() => {callback()}, 2000);
+}
+
+function createMultiplier(val1) {
+return (val2) => {return val1 * val2}
+}
+
+function multiplier(a, b) {
+return a * b;
+}
+
+var doubler = createMultiplier(2);
+var tripler = createMultiplier(3);
+var doublerWithBind = multiplier.bind(null, 2);
+var triplerWithBind = multiplier.bind(null, 3);
